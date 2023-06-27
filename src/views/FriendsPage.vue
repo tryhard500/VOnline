17b5038391039fc91f100fdc814a7f289b6d460c
Nikita Baskov <nb500205@gmail.com>
2023-06-27 15:27:40 +0700

    сделал страницу друзья

--- src/views/FriendsPage.vue
+++ src/views/FriendsPage.vue
@@ -1,14 +1,78 @@
 <script>
-export default {
+import axios from 'axios';
 
+export default {
+    data() {
+        return {
+            friends: []
+        }
+    },
+    mounted() {
+        this.loadFriends();
+    },
+    methods: {
+        async loadFriends() {
+            let response = await axios('/users');
+            this.friends = response.data;
+        },
+        goUser(user) {
+            this.$router.push({
+                name: 'user',
+                params: {
+                    username: user.username
+                }
+            });
+        }
+    }
 }
 </script>
 
+
 <template>
-    Друзья
+    <div class="friends-page">
+        <h3>Друзья</h3>
+
+        <ul class="list-group">
+            <li
+                v-for="(item, index) in friends"
+                @click="goUser(item)"
+                class="list-group-item friend"
+            >
+                <img :src="'src/avatars/' + item.avatar">
+                <div class="username">
+                    {{ item.firstName }} {{ item.lastName }}
+                </div>
+            </li>
+        </ul>
+    </div>
 </template>
 
 
 <style>
+.friends-page {
+    width: 80%;
+    text-align: center;
+}
 
+.friends-page img {
+    width: 80px;
+    height: 80px;
+    object-fit: cover;
+    object-position: center;
+    border-radius: 100%;
+    border: 1px solid #6c757d;
+}
+
+.friends-page .friend {
+    cursor: pointer;
+    height: 100px;
+    
+    display: flex;
+    gap: 20px;
+    align-items: center;
+}
+
+.friends-page .username {
+    font-weight: bold;
+}
 </style>
